97ee5df5b90da7cd6a632fa8096a7f4dae7c95c0
gaobo <gaobo@teddymobile.cn>
2022-05-19 23:56:54 +0800

    deploy

--- js/data.js
+++ js/data.js
@@ -1,1 +1,1 @@
-let dataList=[{"id":5075900,"name":"Cgj5tuiRSjLnpHrVONlLww==","postDate":"2022-05-16"},{"id":5076229,"name":"k8vvoIXI7C2l7bHFmDGICg==","postDate":"2022-05-16"}]
+let dataList=[{"id":5075900,"name":"Cgj5tuiRSjLnpHrVONlLww==","postDate":"2022-05-16"}]
